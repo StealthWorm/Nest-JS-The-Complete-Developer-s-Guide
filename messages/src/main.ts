b84d2477521c255cc1c6479f5a43d4bc9a7ed60d
StealthWorm <thierrypitela@hotmail.com>
2024-02-19 20:45:49 -0300

    feat: validates request data with pipes

--- messages/src/main.ts
+++ messages/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+
+  //  podemos aplicar pipes para rotas isoladas se precisarmos, mas nesse caso queremos validar todas as requisições
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,19 +1,20 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('/messages')
 export class MessagesController {
-  @Get()
+  @Get() // http GET localhost:3000/messages
   listMessages() {
     return 'get response';
   }
 
-  @Post()
-  createMessage() {
-    return 'post response';
+  @Post() // http POST localhost:3000/messages content="hi there"
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
   }
 
-  @Get('/:id')
-  getMessages() {
-    return 'get 1 message response';
+  @Get('/:id') // http GET localhost:3000/messages/1
+  getMessages(@Param('id') id: string) {
+    console.log(id);
   }
 }
