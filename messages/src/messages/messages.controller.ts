0039e474a73707acb00c6165c36e8ed0b3ff6df1
StealthWorm <thierrypitela@hotmail.com>
2024-02-20 20:37:24 -0300

    feat: creates service and repository for the functions of the messages module + use of Dependency Injection Principle

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,20 +1,36 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('/messages')
 export class MessagesController {
+  constructor(public messagesService: MessagesService) { }
+
   @Get() // http GET localhost:3000/messages
   listMessages() {
-    return 'get response';
+    return this.messagesService.findAll();
   }
 
   @Post() // http POST localhost:3000/messages content="hi there"
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id') // http GET localhost:3000/messages/1
-  getMessages(@Param('id') id: string) {
-    console.log(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
   }
 }
