a970e670ed0239d9eecaa08a92822cf4d258982a
StealthWorm <thierrypitela@hotmail.com>
2024-03-30 21:48:05 -0300

    preproduction commit

--- mycv/src/main.ts
+++ mycv/src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   //   whitelist: true, // essa linha garante que o corpo das requisições não possua divergencias com os DTOs, nao possuo propriedades faltando ou a mais
   // }),
   // );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
